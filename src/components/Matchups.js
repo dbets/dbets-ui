7e17894e178db0a3b745ee9ad7d6cc582154cb2d
piz <bpisnoy@gmail.com>
2017-09-12 22:38:06 -0400

    Added test data JSON and loaded into Matchups component.

--- src/components/Matchups.js
+++ src/components/Matchups.js
@@ -1,108 +1,133 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Matchups extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      league: "MLB"
+    };
+  }
+
+  componentWillMount() {
+    var url = "https://api.github.com/users/dBetsPiz";
+    var json = require('../data/testdata.json');
+
+    console.log(json)
+
+    fetch(url).then(d => d.json()).then(d => {
+      d = json
+      this.setState({gamesData: json})
+    })
+
+  }
+
   render() {
-    return (
-      <div id="matchups">
+    var league;
+
+    if (window.location.hash) {
+      league = window.location.hash.substring(1);
+
+    } else {
+      league = this.state.league;
+    }
+
+    if (!this.state.gamesData) {
+      return (
         <div id="left-nav">
-          <ul className="nav nav-pills nav-stacked affix">
-              <li><a href="#NFL">NFL</a></li>
-              <li><a href="#NCAAF">NCAAF</a></li>
-              <li><a href="#NBA">NBA</a></li>
-              <li><a href="#NCAAB">NCAAB</a></li>
-              <li><a href="#MLB">MLB</a></li>
-              <li><a href="#NHL">NHL</a></li>
-            </ul>
+
+          <p>Loading...</p>
         </div>
 
-        <div id="matchups_container">
-          <div id="matchup-header">
+      )
+    } else {
 
-            <table id="table-header">
+      return (
+        <div id="matchups">
+          <div id="left-nav">
+            <ul className="nav nav-pills nav-stacked affix">
+              <li>
+                <a href="#NFL">NFL</a>
+              </li>
+              <li>
+                <a href="#NCAAF">NCAAF</a>
+              </li>
+              <li>
+                <a href="#NBA">NBA</a>
+              </li>
+              <li>
+                <a href="#NCAAB">NCAAB</a>
+              </li>
+              <li>
+                <a href="#MLB">MLB</a>
+              </li>
+              <li>
+                <a href="#NHL">NHL</a>
+              </li>
+            </ul>
+          </div>
+
+          <div id="matchups_container">
+            <div id="matchup-header">
+              <h3>{league}</h3>
+              <table id="table-header">
+                <tbody>
+                  <tr>
+                    <th>Time</th>
+                    <th>Team</th>
+                    <th>Spread</th>
+                    <th>Money Line</th>
+                    <th>Total</th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <table>
+              <thead className="date-row">Sept 10, 2017</thead>
               <tbody>
-                <tr>
-                  <th colSpan="6">NFL</th>
-                </tr>
-                <tr>
-                  <th>Time</th>
-                  <th>Team</th>
-                  <th>Spread</th>
-                  <th>Money Line</th>
-                  <th>Total</th>
-                </tr>
+                {this.state.gamesData.map(function(game) {
+                  return <tr className="game-row">
+                    <table className="games-table">
+                      <tr>
+                        <td className="colTime" rowSpan="2">{game.MatchTime}</td>
+                        <td className="colTeam">{game.AwayTeam}</td>
+                        <td className="colSpread">
+                          <button>{game.Odds[0].PointSpreadAway}  ({game.Odds[0].PointSpreadAwayLine})</button>
+                        </td>
+                        <td className="colML">
+                          <button>{game.Odds[0].MoneyLineAway}</button>
+                        </td>
+                        <td className="colTotal">
+                          <button>{game.Odds[0].TotalNumber} ({game.Odds[0].OverLine})</button>
+                        </td>
+                        <td className="colButton" rowSpan="2">
+                          <button>All Bets</button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="colTeam">{game.HomeTeam}</td>
+                        <td className="colSpread">
+                          <button>{game.Odds[0].PointSpreadHome}  ({game.Odds[0].PointSpreadHomeLine})</button>
+                        </td>
+                        <td className="colML">
+                          <button>{game.Odds[0].MoneyLineHome}</button>
+                        </td>
+                        <td className="colTotal">
+                          <button>{game.Odds[0].TotalNumber}  ({game.Odds[0].UnderLine})</button>
+                        </td>
+                      </tr>
+                    </table>
+                  </tr>
+
+                })
+}
               </tbody>
             </table>
           </div>
-
-          <table>
-            <thead className="date-row">Sept 10, 2017</thead>
-            <tbody>
-
-              <tr className="game-row">
-                <table className="games-table">
-                  <tr>
-                    <td className="colTime" rowSpan="2">7:00PM</td>
-                    <td className="colTeam" >Away</td>
-                    <td className="colSpread" >
-                      <button>+3</button>
-                    </td>
-                    <td className="colML" >
-                      <button>+125</button>
-                    </td>
-                    <td className="colTotal" >
-                      <button>51</button>
-                    </td>
-                    <td className="colButton" rowSpan="2"><button>All Bets</button></td>
-                  </tr>
-                  <tr>
-                    <td className="colTeam" >Home</td>
-                    <td className="colSpread" >
-                      <button>+3</button>
-                    </td>
-                    <td className="colML" >
-                      <button>+125</button>
-                    </td>
-                    <td className="colTotal" >
-                      <button>51</button>
-                    </td>
-                  </tr>
-                </table>
-              </tr>
-              <tr className="game-row">
-                <table className="games-table">
-                  <tr>
-                    <td className="colTime" rowSpan="2">7:00PM</td>
-                    <td className="colTeam" >Away</td>
-                    <td className="colSpread" >
-                      <button>+3</button>
-                    </td>
-                    <td className="colML" >
-                      <button>+125</button>
-                    </td>
-                    <td className="colTotal" >
-                      <button>51</button>
-                    </td>
-                    <td className="colButton" rowSpan="2"><button>All Bets</button></td>
-                  </tr>
-                  <tr>
-                    <td className="colTeam" >Home</td>
-                    <td className="colSpread" >
-                      <button>+3</button>
-                    </td>
-                    <td className="colML" >
-                      <button>+125</button>
-                    </td>
-                    <td className="colTotal" >
-                      <button>51</button>
-                    </td>
-                  </tr>
-                </table>
-              </tr>
-            </tbody>
-          </table>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,32 +6,36 @@ import Nav from './components/Nav'
 import Home from './components/Home'
 import Matchups from './components/Matchups'
 import WagerBoard from './components/WagerBoard'
-import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
-    <div className="container">
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/">dBets</a>
-          </div>
-            <ul className="nav navbar-nav">
-              <li><NavLink activeClassName="activeNav" to="/matchups">Matchups</NavLink></li>
-              <li><NavLink activeClassName="activeNav" to="/wagerboard">WagerBoard</NavLink></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><NavLink activeClassName="activeNav" to="/account">My Account</NavLink></li>
-            </ul>
+  <div className="container">
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="/">dBets</a>
         </div>
-      </nav>
-
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/matchups" component={Matchups}/>
-        <Route exact path="/wagerboard" component={WagerBoard}/>
+        <ul className="nav navbar-nav">
+          <li>
+            <NavLink activeClassName="activeNav" to="/matchups">Matchups</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeNav" to="/wagerboard">WagerBoard</NavLink>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink activeClassName="activeNav" to="/account">My Account</NavLink>
+          </li>
+        </ul>
       </div>
-  </Router>,
-  document.getElementById('root')
-);
+    </nav>
+
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/matchups" component={Matchups}/>
+    <Route exact path="/wagerboard" component={WagerBoard}/>
+  </div>
+</Router>, document.getElementById('root'));
 registerServiceWorker();
